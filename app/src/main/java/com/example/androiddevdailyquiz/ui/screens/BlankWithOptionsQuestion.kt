333e8673e04aa1827d2f9800727734bb4b6fdeae
CNJerry-IvanovVyacheslav <syava11212@yandex.ru>
2025-10-14 12:15:27 +0400

    Fixed an error with incorrect calculation of the correct answer to the first question.

--- app/src/main/java/com/example/androiddevdailyquiz/ui/screens/BlankWithOptionsQuestion.kt
+++ app/src/main/java/com/example/androiddevdailyquiz/ui/screens/BlankWithOptionsQuestion.kt
@@ -1,16 +1,27 @@
 package com.example.androiddevdailyquiz.ui.screens
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.key
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.androiddevdailyquiz.data.model.Question
+
 
 @Composable
 fun BlankWithOptionsQuestion(
@@ -19,6 +30,7 @@ fun BlankWithOptionsQuestion(
 ) {
     key(question.id) {
         var selectedOption by remember { mutableStateOf<String?>(null) }
+        var isCorrectAnswer by remember { mutableStateOf<Boolean?>(null) }
 
         val options = remember(question.id) {
             val all = mutableListOf(question.answer)
@@ -52,8 +64,8 @@ fun BlankWithOptionsQuestion(
                     option.trim().equals(question.answer.trim(), ignoreCase = true)
 
                 val containerColor = when {
-                    isSelected && isAnswerCorrect -> MaterialTheme.colorScheme.primary
-                    isSelected && !isAnswerCorrect -> MaterialTheme.colorScheme.errorContainer
+                    isSelected && isCorrectAnswer == true -> MaterialTheme.colorScheme.primary
+                    isSelected && isCorrectAnswer == false -> MaterialTheme.colorScheme.errorContainer
                     selectedOption != null && isAnswerCorrect -> MaterialTheme.colorScheme.primary.copy(
                         alpha = 0.5f
                     )
@@ -70,7 +82,7 @@ fun BlankWithOptionsQuestion(
                         .padding(vertical = 6.dp)
                         .clickable(enabled = selectedOption == null) {
                             selectedOption = option
-                            onCheckAnswer(option)
+                            isCorrectAnswer = onCheckAnswer(option)
                         }
                 ) {
                     Box(
